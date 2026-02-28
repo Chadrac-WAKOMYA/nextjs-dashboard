d67e84620b98a2d8d29c2a77ff0ffa01cd12658a
Chadrac-WAKOMYA <chadracxx@gmail.com>
2026-02-28 10:54:10 +0100

    feat: ready to continu

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -22,7 +22,6 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         ]}
       />
       <Form invoice={invoice} customers={customers} />
-      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
